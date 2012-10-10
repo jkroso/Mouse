797371b0e51f21833e7ec5244b7e9e2be6dc3c85
Jakeb Rosoman <jkroso@gmail.com>
2012-10-11 09:41:40 +1300

    now using Context instead of jQuery

--- demo/demo.js
+++ demo/demo.js
@@ -1,29 +1,29 @@
 require.config({
     baseUrl: '../',
     shim : {
-        Adept: {
+        jQuery: {
             exports: '$'
         }
     },
     paths : {
-        Adept: '/Libraries/jquery-latest'
+        jQuery: '/Libraries/jquery-latest'
     }
 })
 // This example is working with jquery. Though the library was originally designed to work with adept.js. It would also work with Ender.js, Zepto and others
-require(['Adept', 'src/Mouse', '../Dom411/src/Dom411'], function ($) {
+require(['../Dom411/src/Dom411', 'jQuery', 'src/Mouse'], function (Dom411, $) {
     var total, timer, top, left
 
     // This causes just a single dom event listener
-    $('#list1')
-        .forward('.item', 'grab.left', start)
-        .forward('.item', 'drag.left', move)
-        .forward('.item', 'drop.left', stop)
+    Dom411('#list1')
+        .delegate('.item', 'grab.left', start)
+        .delegate('.item', 'drag.left', move)
+        .delegate('.item', 'drop.left', stop)
     
     // This causes one listener to be bound to each list item
-    $('#list2 > .item')
-        .subscribe('grab.left', start)
-        .subscribe('drag.left', move)
-        .subscribe('drop.left', stop)
+    Dom411('#list2 > .item')
+        .on('grab.left', start)
+        .on('drag.left', move)
+        .on('drop.left', stop)
 
     function start (e) {
         timer = e.timeStamp
@@ -33,18 +33,15 @@ require(['Adept', 'src/Mouse', '../Dom411/src/Dom411'], function ($) {
     }
     function stop (e) {
         var duration = e.timeStamp - timer
-        $('#count').html(Math.round(total / (duration / 1000)))
+        document.getElementById('count').innerHTML = Math.round(total / (duration / 1000))
     }
     function move (e) {
         total++
-        $(this).css({
-            top: (top += e.movementY) + 'px',
-            left: (left += e.movementX) + 'px'
-        })
+        this.style.top = (top += e.movementY) + 'px'
+        this.style.left = (left += e.movementX) + 'px'
     }
 
     // Using only native events, though, enhanced by jquery
-    // Note I wrote two bugs when writing the following code despite having written similar code many times before. Hence the library.
     $('#list3 > .item').on('mousedown', function (e) {
         start.call(this, e)
         lastEvent = e
@@ -56,13 +53,12 @@ require(['Adept', 'src/Mouse', '../Dom411/src/Dom411'], function ($) {
                 $(document.body).off('mousemove', dragHandler)
             })
     })
+
     var lastEvent
     function jqMove (e) {
         total++
-        $(this).css({
-            top: (top += e.clientY - lastEvent.clientY) + 'px',
-            left: (left += e.clientX - lastEvent.clientX) + 'px'
-        })
+        this.style.top = (top += e.clientY - lastEvent.clientY) + 'px'
+        this.style.left = (left += e.clientX - lastEvent.clientX) + 'px'
         lastEvent = e
     }
 })
